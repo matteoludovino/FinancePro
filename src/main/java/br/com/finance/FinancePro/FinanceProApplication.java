3cfe4a06c8944f20cb3cb9728f808db970824831
Victor Pascoal <162490483+VictorPascoal@users.noreply.github.com>
2025-03-24 19:18:32 -0300

    First

---
+++ src/main/java/br/com/finance/FinancePro/FinanceProApplication.java
@@ -0,0 +1,13 @@
+package br.com.finance.FinancePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/finance/FinancePro/FinanceProApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finance.FinancePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
